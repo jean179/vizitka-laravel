2abfb5d3531b549688b8caeee0ae662dc3fea2dd
jean179 <jean179@mail.ru>
2015-03-30 09:30:40 +0300

    Установка модулей для Ларавер: ide-helper, way-generator, facker

--- app/config/local/database.php
+++ app/config/local/database.php
@@ -22,9 +22,9 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
-			'database'  => 'homestead',
-			'username'  => 'homestead',
+			'host'      => '127.0.0.1',
+			'database'  => 'root',
+			'username'  => '',
 			'password'  => 'secret',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
